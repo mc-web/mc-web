9af627efa41f4d0fa741959299f4ec01c5726602
liuwuwei <liuwuwei@shenjianshou.cn>
2020-05-19 19:31:19 +0800

    调整代码结构 修改texture配置

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 import {ImprovedNoise as improved_noise} from "./libs/ImprovedNoise.js";
 const WORLD_WIDTH = 20;
 const WORLD_LENGTH = 20;
-
-
+const a = 1; //方块边长
 const screen_width = document.documentElement.clientWidth,
     screen_height = document.documentElement.clientHeight;
-    
+
+
+
+// init
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(screen_width, screen_height);
 document.getElementsByTagName('body')[0].appendChild(renderer.domElement);
@@ -14,73 +16,94 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color( 0xbfd1e5 );
 
 const camera = new THREE.PerspectiveCamera(45, screen_width / screen_height, 0.1, 1000);
-camera.position.set(0,15,0);
+camera.position.set(0,10,0);
 camera.lookAt(new THREE.Vector3(0, 0, 0));
 scene.add(camera);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1000);
 scene.add(directionalLight);
 
-let geometry = new THREE.BoxGeometry(3, 3, 3);
-let url_list = ["./textures/grass_dirt.png", "./textures/grass_dirt.png",
-                "./textures/grass.png", "./textures/dirt.png",
-                "./textures/grass_dirt.png", "./textures/grass_dirt.png"];
-let materials = [];
-for(let url of url_list) {
-    materials.push(
-        new THREE.MeshBasicMaterial({
-            map: new THREE.TextureLoader().load(url),
-            side: THREE.DoubleSide,
-        })
-    );
-}
+
+
+// generate cubes
+let geometry = new THREE.BoxGeometry(a, a, a);
+
+let grass_dirt_materials = generateMaterialArr([
+    "./textures/grass_dirt.png", "./textures/grass_dirt.png",
+    "./textures/grass.png", "./textures/dirt.png",
+    "./textures/grass_dirt.png", "./textures/grass_dirt.png"
+]);
+
+let dirt_materials = generateMaterialArr([
+    "./textures/dirt.png","./textures/dirt.png","./textures/dirt.png",
+    "./textures/dirt.png","./textures/dirt.png","./textures/dirt.png"
+]);
 
 let heights_arr = generateHeight(WORLD_WIDTH, WORLD_LENGTH);
 for(let i = 0; i < WORLD_LENGTH; i++) {
     for(let j = 0; j < WORLD_WIDTH; j++) {
         for(let h = 0; h < Math.abs(heights_arr[i * WORLD_WIDTH + j] * 0.2 | 0); h++) {
-            let cube2 = new THREE.Mesh(geometry, materials);
-            cube2.position.x = i;
-            cube2.position.y = h;
-            cube2.position.z = j;
-            scene.add(cube2);
+            let cube;
+            if(h === Math.abs(heights_arr[i * WORLD_WIDTH + j] * 0.2 | 0) - 1) {
+                // grass_dirt
+                cube = new THREE.Mesh(geometry, grass_dirt_materials);
+            } else {
+                // grass
+                cube = new THREE.Mesh(geometry, dirt_materials);
+            }
+
+            cube.position.x = i * a;
+            cube.position.y = h * a;
+            cube.position.z = j * a;
+            scene.add(cube);
         }
     }
 }
 
-console.log(scene)
 renderer.render(scene, camera);
 
 let orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
-console.log(orbitControls)
 window.requestAnimationFrame(step);
 function step() {
     renderer.render(scene, camera);
-    console.log(1)
     window.requestAnimationFrame(step)
 }
 
-
-function generateHeight( width, height ) {
+function generateHeight(width, height) {
     let data = [], perlin = new improved_noise(),
         size = width * height, 
         quality = 2, 
         z = Math.random() * 100;
 
-    for (let j = 0; j < 4; j ++) {
+    for (let j = 0; j < 4; j++) {
         if (j === 0) {
-            for (let i = 0; i < size; i ++) {
-                data[ i ] = 0;
+            for (let i = 0; i < size; i++) {
+                data[i] = 0;
             }
         }
-        for (let i = 0; i < size; i ++) {
+        for (let i = 0; i < size; i++) {
             let x = i % width, 
-                y = ( i / width ) | 0;
+                y = (i / width) | 0;
             data[i] += perlin.noise(x / quality, y / quality, z) * quality;
         }
         quality *= 4;
     }
 
     return data;
+}
 
+function generateMaterialArr(texture_arr) {
+    let materials = [];
+    for(let url of texture_arr) {
+        let texture = new THREE.TextureLoader().load(url);
+        texture.magFilter = THREE.NearestFilter;
+        texture.minFilter = THREE.NearestFilter;
+        materials.push(
+            new THREE.MeshBasicMaterial({
+                map: texture,
+                side: THREE.DoubleSide,
+            })
+        );
+    }
+    return materials;
 }
